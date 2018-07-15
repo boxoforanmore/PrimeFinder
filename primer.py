473eddaecd21a899de48f9c91adbd5709b66dc1d
Peter Nielson <computergeekpete@gmail.com>
2018-07-15 13:11:14 -0600

    Added file read/write tests

--- primer.py
+++ primer.py
@@ -4,10 +4,11 @@ import csv
 
 class PrimeNumbers():
     def __init__(self, lower = 2, upper = 10, filename = 'primesList.log'):
-        self.__filename = filename
+        self.__filename = str(filename)
         self.__primes = []
-        self.upper = upper
-        self.lower = lower
+        self.upper = int(upper)
+        self.lower = int(lower)
+        self.__validRead = False
 
         ## Fix error messages
         if self.upper <= 1:
@@ -29,15 +30,19 @@ class PrimeNumbers():
 
     ## add a break for if prime is more than halfway through the list
     def __is_prime(self, num):
+        count = 0
         for prime in self.__primes:
             if((num % prime) == 0):
                 return False
+            if count > (len(self.__primes) // 2):
+                break
+            count += 1
         return True
 
     def __from_file(self):
-        with open(self.__filename, "r") as inputNumbers:
-            for item in inputNumbers:
-                pass
+        with open(self.__filename, "w+") as inputNumbers:
+            for line in inputNumbers:
+                self.__primes.append(int(line))
 
     def add_primes(self):
         self.__from_file()
@@ -45,18 +50,18 @@ class PrimeNumbers():
             for i in range(2, self.lower):
                 if self.__is_prime(i):
                     self.__primes.append(i)
+        else:
+            self.__validRead = True
         if len(self.__primes) > 0:
             self.lower = self.__primes[len(self.__primes) - 1] + 1
         for num in range(self.lower, self.upper):
             if self.__is_prime(num):
                 self.__primes.append(num)
 
-    def print_primes_into_filei(self):
-
-        for prime in self.__primes:
-            pass
-            #print("%d," prime)
-
+    #def print_primes_into_filei(self):
+        #for prime in self.__primes:
+        #    pass
+        #    #print("%d," prime)
 
     def __str__(self):
         temp = []
@@ -65,6 +70,26 @@ class PrimeNumbers():
                 temp.append(i)
         return str(temp)
 
+
+    def close(self):
+        self.__write_to_file()
+        return
+
+    def __write_to_file(self):
+        if self.__validRead == True:
+            with open(self.__filename, "a") as appendTo:
+                for i in self.__primes:
+                    if i >= self.lower:
+                        appendTo.write("%d\n" % i)
+                appendTo.close()
+                return
+        else:
+            with open(self.__filename, "w+") as writeTo:
+                for i in self.__primes:
+                    writeTo.write("%d\n" % i)
+                writeTo.close()
+                return
+                
 
 class Test_Primes(unittest.TestCase):
     def test_zero_upper(self):
@@ -91,7 +116,41 @@ class Test_Primes(unittest.TestCase):
         primes.add_primes()
         self.assertEqual("[23, 29, 31, 37, 41, 43, 47]", str(primes))
 
+    def test_write_default_range(self):
+        test = "test.log"
+        testList = []
+        primes = PrimeNumbers(filename=test)
+        primes.add_primes()
+        primes.close()
+        with open(test, "r") as reader:
+            for line in reader:
+                testList.append(int(line))
+        self.assertEqual([2, 3, 5, 7], testList)
 
+    def test_append_to_default(self):
+        test = "test.log"
+        testList = []
+        primes = PrimeNumbers(lower=20, upper=50, filename=test)
+        primes.add_primes()
+        primes.close()
+        with open(test, "r") as reader:
+            for line in reader:
+                testList.append(int(line))
+        self.assertEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47], \
+                          testList)
 
-if '__main__' == __name__:
-    unittest.main()
+    def test_write_upper_range(self):
+        test = "test1.log"
+        testList = []
+        primes = PrimeNumbers(lower=20, upper=50, filename=test)
+        primes.add_primes()
+        primes.close()
+        with open(test, "r") as reader:
+            for line in reader:
+                testList.append(int(line))
+        self.assertEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47], \
+                          testList)
+
+#if '__main__' == __name__:
+#    unittest.main()
+
